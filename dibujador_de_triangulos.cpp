304450b9d49efb9106aa0be4fb3dbe4e41fa2106
ger204 <gerardmt04@gmail.com>
2018-07-05 23:01:38 +0200

    Code for drow a triangle :)

--- dibujador_de_triangulos.cpp
+++ dibujador_de_triangulos.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 using namespace std;
 int main() {
-  std::cout << "Hello world" << '\n';
+  int h;
+  std::cout << "INGRESE LA ALTURA y EL TRIANGLE:";
+  std::cin >> h;
+
+  for (int i = 1; i <= h; i++) {
+    for (int e = 1; e <= i; e++) {
+      std::cout << "*";
+    }
+    std::cout<< '\n';
+  }
+
+  /*
+  i=1
+  e=1
+  *
+  i=2
+  e=1
+  **
+  i=3
+  e=1
+  ***
+  i=4
+  e=1
+  ****
+    */
   return 0;
 }
